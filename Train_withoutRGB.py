ca5287f9723f681d64501e1568d275d6003cd846
AghaFarshid <faarshidbaakhshi@gmail.com>
2024-12-17 16:03:33 +0100

    Update

--- Train_withoutRGB.py
+++ Train_withoutRGB.py
@@ -9,7 +9,7 @@ from pointnet_ import PointNet2ClsSSG
 import logging
 
 # === Configure Logging ===
-log_file_path = "/content/drive/MyDrive/t1/training_logsNORGB.txt"
+log_file_path = "/content/drive/MyDrive/training_logsNORGB.txt"
 logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s - %(message)s')
 
 def log_and_print(message):
