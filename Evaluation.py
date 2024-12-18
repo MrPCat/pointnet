a40cc402b930c876d7f00c9f71f6e50485cfd5b6
AghaFarshid <faarshidbaakhshi@gmail.com>
2024-12-18 23:29:34 +0100

    Update

--- Evaluation.py
+++ Evaluation.py
@@ -135,8 +135,8 @@ def main(txt_file, las_file):
     evaluate_classes(merged_data)
 
 # Replace with your actual file paths
-txt_file = r"C:\Users\faars\Downloads\PredictTest_noRGB.txt"
-las_file = r"C:\Farshid\Uni\Semesters\Thesis\Data\Epoch_March2018\LiDAR\Mar18_test_GroundTruth.las"
+txt_file = r"/content/drive/MyDrive/t1/Mar18_testWithoutRGB_predictions.txt"
+las_file = r"/content/drive/MyDrive/t1/Mar18_test_GroundTruth.las"
 
 # Run the comparison
 main(txt_file, las_file)
