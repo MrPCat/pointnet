974480e6f1dd4a4e397e5f703b54e2c0850dddb1
AghaFarshid <faarshidbaakhshi@gmail.com>
2024-12-29 16:07:10 +0100

    Update

--- Train_withoutRGB.py
+++ Train_withoutRGB.py
@@ -145,7 +145,6 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     log_and_print(f"Using device: {device}")
 
-    # Verify directory path
     dir_path = '/content/drive/MyDrive/Vaihingen'
     if not os.path.exists(dir_path):
         raise ValueError(f"Directory does not exist: {dir_path}")
